c6540acaee6e626e23fac38fd8224a1c2f418542
Angel Murchison <angelmurchison09@gmail.com>
2017-04-10 19:07:46 -0400

    Reorganized and renamed weapon properties

--- dks3Api/Models/Weapon.cs
+++ dks3Api/Models/Weapon.cs
@@ -1,25 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace dks3Api.Models
 {
+
     public class Weapon
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Class { get; set; }
-        public string DamageType { get; set; }
         public double Weight { get; set; }
         public int Durability { get; set; }
 
+        #region WeaponProps
+        [Display(Name = "Damage Type")]
+        public string DamageType { get; set; }
+        [Display(Name = "Weapon Class")]
         public WeaponClass WeaponClass { get; set; }
-        public WeaponArts WeaponArts { get; set; }
+        [Display(Name = "Weapon Art")]
+        public WeaponArt WeaponArt { get; set; }
+        [Display(Name = "Attack Power")]
         public AttackPower AttackPower { get; set; }
-        public GuardAbsorption GuardAbsorption { get; set; }
-        public AttributeBonus AttributeBonus { get; set; }
+        [Display(Name = "Damage Absorption")]
+        public DamageAbsorption DamageAbsorption { get; set; }
+        [Display(Name = "Damage Scaling")]
+        public DamageScaling DamageScaling { get; set; }
+        [Display(Name = "Additional Effects")]
         public AdditionalEffects AdditionalEffects { get; set; }
-        public AttributeRequirement AttributeRequirement { get; set; }
+        [Display(Name = "Stat Requirements")] public StatRequirements StatRequirement { get; set; }
+        #endregion
     }
 }
--- dks3Api/Models/WeaponProps.cs
+++ dks3Api/Models/WeaponProps.cs
@@ -6,6 +6,7 @@ using System.Web;
 
 namespace dks3Api.Models
 {
+    // Defining all of the properties that weapons share.
     public enum WeaponClass
     {
         Dagger,
@@ -17,7 +18,7 @@ namespace dks3Api.Models
         Katana
     }
 
-    public class WeaponArts
+    public class WeaponArt
     {
         [Display(Name = "Weapon Arts Type")]
         public string WeaponArtsType { get; set; }
@@ -26,21 +27,24 @@ namespace dks3Api.Models
         [Display(Name = "FP Cost")]
         public string FPcost { get; set; }
     }
-    public class AttributeRequirement
+
+    public class StatRequirements
     {
         public int Strength { get; set; }
         public int Dexterity { get; set; }
         public int Intelligence { get; set; }
         public int Faith { get; set; }
     }
-    public class AttributeBonus
+
+    public class DamageScaling
     {
-        public string Strength { get; set; }
-        public string Dexterity { get; set; }
-        public string Intelligence { get; set; }
-        public string Faith { get; set; }
+        public double Strength { get; set; }
+        public double Dexterity { get; set; }
+        public double Intelligence { get; set; }
+        public double Faith { get; set; }
     }
-    public class GuardAbsorption
+
+    public class DamageAbsorption
     {
         public double Physical { get; set; }
         public double Magic { get; set; }
@@ -49,6 +53,7 @@ namespace dks3Api.Models
         public double Dark { get; set; }
         public int Stability { get; set; }
     }
+
     public class AttackPower
     {
         public int Physical { get; set; }
@@ -58,11 +63,18 @@ namespace dks3Api.Models
         public int Dark { get; set; }
         public int Critical { get; set; }
     }
+
     public class AdditionalEffects
     {
         public int Bleed { get; set; }
         public int Poison { get; set; }
         public int Frost { get; set; }
+    }
+
+    public class Moveset
+    {
+        public int ID { get; set; }
+        public string URL { get; set; }
     }
 
 }
